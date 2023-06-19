74b4fe94f625276ea4c653fbe813bba762f5b98d
Eugene Hlushak <hlushak.e@gmail.com>
2023-06-19 23:16:14 +0300

    add edit

--- db/models/user-model.js
+++ db/models/user-model.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt");
-
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    avatarURL: {
-      type: String,
-    },
-    token: {
-      type: String,
-    },
-  },
-  { versionKey: false }
-);
-
-userSchema.methods.setPassword = async function (password) {
-  this.password = await bcrypt.hash(password, 10);
-};
-
-userSchema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.setToken = function (token) {
-  this.token = token;
-};
-
-const User = model("user", userSchema);
-
-module.exports = User;
-=======
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
 
@@ -80,4 +36,3 @@ userSchema.methods.comparePassword = async function (password) {
 const User = model("user", userSchema);
 
 module.exports = User;
->>>>>>> main
